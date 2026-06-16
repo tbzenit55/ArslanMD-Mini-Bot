a9fe854640d6358514166ceedfe3ecb7309482b6
tbzenit55 <ggbbb2561@gmail.com>
2026-06-16 19:46:12 +0530

    Update alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -52,7 +52,7 @@ cmd({
                 isForwarded: true,
                 forwardedNewsletterMessageInfo: {
                     newsletterJid: '120363348739987203@newsletter',
-                    newsletterName: 'ᴀʀꜱʟᴀɴ-ᴍᴅ',
+                    newsletterName: 'Zenit-Xᴍᴅ',
                     serverMessageId: 143
                 }
             }
